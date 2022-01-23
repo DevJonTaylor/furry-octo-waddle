bb9bcf6324c037c9dad0e8ed2da2de1f2b65a741
Jon Taylor <31380713+DevJonTaylor@users.noreply.github.com>
2022-01-22 22:41:06 -0600

    Implemented jQuery and Bootstrap, added a toast component for correct and incorrect answers, redefined JS code.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,364 +1,345 @@
-const _transition = {
-  time: 300,
-  delay: 5,
-  cls: 'transition',
-  timeOut(callback) {
-    const timeout = this.time + this.delay;
-    return setTimeout(callback, timeout);
+/**
+ * The questions are contained inside an array which houses objects.  These objects are then passed to the
+ * Question class.
+ * @type {[{solution: number, questions: string, options: string[]},{solution: number, questions: string, options: string[]},{solution: number, questions: string, options: string[]},{solution: number, questions: string, options: string[]},{solution: number, questions: string, options: string[]},null,null,null,null,null]}
+ * @private
+ */
+const _questions = [
+  {
+    question: 'Which word is not a keyword in JavaScript?',
+    options: ['while', 'for', 'true', 'live'],
+    solution: 3
+  },
+  {
+    question: 'Which word is not a valid method to create a function in JavaScript?',
+    options: ['const a = a => {...}', '(() => { ... })()', 'callback ()', 'function t() {...}'],
+    solution: 2
+  },
+  {
+    question: 'This works in IE8:',
+    options: ['fetch', 'Intl.Locale', 'console.debug', 'XMLHttpRequest'],
+    solution: 3
+  },
+  {
+    question: 'JavaScript went live in what year?',
+    options: ['1900', '2020', '1995', '1993'],
+    solution: 2
+  },
+  {
+    question: 'You can convert a number to a string by using?',
+    options: ['Int2String', 'Stringify', '2String', 'toString'],
+    solution: 3
+  },
+  {
+    question: 'Which word is not a keyword in JavaScript?',
+    options: ['while', 'for', 'true', 'live'],
+    solution: 3
+  }
+]
+
+/**
+ *
+ * @property {number} time The current time on the timer.
+ * @property {number} interval The interval ID that is returned from setInterval function.
+ * @property {HTMLElement} el The element that will display the current time left.
+ */
+class TimeManagement {
+  #tickTime = 1000;
+  #start = 75;
+
+  /**
+   * Starts a timer based on the private options.
+   */
+  constructor() {
+    // The time is set based on the private property #start.
+    this.time = 0;
+
+    // Bind the method so that when it is called *this* is still TimeManagement.
+    // #tickTime is a private property that defines how fast the tick method will run.
+    this.interval = 0;
+
+    // This is the element that will display the current time remaining.
+    this.el = document.querySelector('#timer');
+
+    this.events = {
+      tick: [],
+      end: [],
+      start: []
+    }
+  }
+
+  /**
+   * This sets an event handler to run at the end of the timer.
+   * @param {function} cb The function to handle the event.
+   */
+  onEnd(cb) {
+    this.events.end.push(cb);
+  }
+
+  /**
+   * A event handler for the start of the timer.
+   * @param {function} cb The function to handle the event.
+   */
+  onStart(cb) {
+    this.events.start.push(cb);
+  }
+
+  /**
+   * An event handler to run when a tick runs.
+   * @param {function} cb The function to handle the event.
+   */
+  onTick(cb) {
+    this.events.tick.push(cb);
+  }
+
+  /**
+   * Starts or resets the time and interval.
+   */
+  start() {
+    this.time = this.#start;
+    this.display();
+    this.interval = setInterval(this.tick.bind(this), this.#tickTime);
+    this.emit('start', this.el);
+  }
+
+  emit(eventName, ...args) {
+    for(let handler of this.events[eventName]) {
+      handler(...args);
+    }
+  }
+
+  /**
+   * A way to ensure that display is the same and scalable if needed.
+   */
+  display() {
+    this.el.innerText = this.time;
+  }
+
+  /**
+   * This method tells the timer what to do every tick.
+   */
+  tick() {
+    if(this.time <= 0) {
+      this.stopTimer();
+    }
+
+    this.display();
+    this.subtractTime(1);
+    this.emit('tick', this.el, this.time);
+  }
+
+  /**
+   * This method stops the timer regardless if the time on the clock is zero.
+   */
+  stopTimer() {
+    clearInterval(this.interval);
+    this.display();
+    this.emit('end', this.el);
+  }
+
+  /**
+   * Subtracts a specific amount from the timer.
+   * @param {number} n Amount to be subtracted.
+   */
+  subtractTime(n) {
+    if(n >= this.time) this.time = 0;
+    else this.time -= n;
   }
 }
-const _timer = {
-  interval: 1000,
-  start: 10,
-  tick: 1,
-  incorrect: 10
-}
-const q1 = {
-    question: "Questions 1",
-    options: [
-      "Option 1",
-      "Option 2",
-      "Option 3",
-      "Option 4"
-    ],
-    solution: 2
-}, q2 = {
-    question: "Questions 2",
-    options: [
-      "Option 1",
-      "Option 2",
-      "Option 3",
-      "Option 4"
-    ],
-    solution: 2
-}, q3 = {
-    question: "Questions 3",
-    options: [
-      "Option 1",
-      "Option 2",
-      "Option 3",
-      "Option 4"
-    ],
-    solution: 2
-}, q4 = {
-    question: "Questions 4",
-    options: [
-      "Option 1",
-      "Option 2",
-      "Option 3",
-      "Option 4"
-    ],
-    solution: 2
-}, q5 = {
-    question: "Questions 5",
-    options: [
-      "Option 1",
-      "Option 2",
-      "Option 3",
-      "Option 4"
-    ],
-    solution: 2
-  }
 
-class Question {
-  #answerProvided = null;
-
-  constructor({question, options, solution}) {
-    this.question = question;
-    this.solution = solution;
-    this.options = options;
-  }
-
-  get answer() {
-    return this.#answerProvided;
-  }
-
-  set answer(answer) {
-    this.#answerProvided = parseInt(answer);
-  }
-
-  get isCorrect() {
-    return this.answer === this.solution;
-  }
-
-  get isAnswered() {
-    return this.answer !== null;
-  }
-}
-
-class QuizManager {
-  #container;
-  #timer;
-  #endHigh;
-  #updateScore;
-  constructor($_Container, timer, $_endHigh) {
-    this.#timer = timer;
-    this.#container = $_Container;
-    this.questions = [];
+class QuizCard {
+  constructor(questions) {
+    this.questions = questions;
+    this.$ = $('#quiz');
     this.currentIndex = 0;
-    this.#endHigh = $_endHigh;
+    this.answers = [];
+  }
+
+  get current() {
+    return this.questions[this.currentIndex]
   }
 
   get correct() {
     let correct = 0;
-    for(let question of this.questions) {
-      if(question.isCorrect) correct++;
-    }
+    this.#iterateThroughAnswers((solution, answered) => {
+      if(solution === answered) correct++;
+    })
 
     return correct;
   }
 
-  get score() {
-    return `${this.correct / this.questions.length * 100}%`;
-  }
-
-  get current() {
-    return this.questions[this.currentIndex];
-  }
-
-  get options() {
-    return this.current.options;
-  }
-
-  set answer(answer) {
-    this.current.answer = answer;
-    this.updateScore();
-    if(!this.current.isCorrect)
-      this.#timer.subtract(_timer.incorrect);
-    this.currentIndex++;
-    this.flipOut()
-      .then(() => {
-        if(this.currentIndex < this.questions.length) {
-          this.update()
-          return this.flipIn();
-        } else {
-          return this.flipIn(this.#endHigh)
-        }
-      });
-  }
-
-  updateScore(_$) {
-    const noUpdater = this.#updateScore === undefined;
-    const noArg = _$ === undefined;
-    if(noArg && noUpdater) return
-    if(noUpdater) {
-      this.#updateScore = _$;
-    }
-
-    this.#updateScore.text(this.score);
-  }
-
-  flipOut($_ = this.#container) {
-    return new Promise(res => {
-      $_.addClass(_transition.cls);
-      _transition.timeOut(() => {
-        $_.hide();
-        res();
-      });
-    });
-  }
-
-  flipIn($_ = this.#container) {
-    return new Promise(res => {
-      $_.show();
-      $_.removeClass(_transition.cls);
-      _transition.timeOut(res);
+  get incorrect() {
+    let incorrect = 0;
+    this.#iterateThroughAnswers((solution, answered) => {
+      if(solution !== answered) incorrect++;
     })
+
+    return incorrect;
   }
 
-  start($_start) {
-    this.reset();
-    this.flipOut($_start)
-      .then(() => {
-        this.update();
-        return this.flipIn();
-      })
-      .then(() => {
-        this.#timer.start();
-      })
+  #iterateThroughAnswers(callback) {
+    for(let i in this.answers) {
+      let solution = this.questions[i].solution;
+      let answered = this.answers[i];
+      callback(solution, answered);
+    }
   }
 
-  update() {
-    let h3 = this.#container.children('h3');
-    let buttons = this.#container.children('.btn');
-
-    h3.text(this.current.question);
-
-    buttons.forEach((node, i) => {
-      node.innerText = this.options[i];
-      node.dataset.option = i;
-    });
+  /**
+   * Checks the answer provided and adds it to the array of answers.
+   * @param {string} answer numeric value of what the answer was.
+   * @returns {boolean} Was the answer correct.
+   */
+  answerQuestion(answer) {
+    const answerInt = parseInt(answer);
+    const correctAnswer = this.questions[this.currentIndex].solution;
+    const answeredCorrectly = correctAnswer === answerInt;
+    this.answers.push(answerInt);
+    this.currentIndex++;
+    return answeredCorrectly;
   }
 
-  addQuestion({question, solution, options}) {
-    this.questions.push(new Question({question, solution, options}))
+  display() {
+    // If the currentIndex is the same as the length of the questions then we have gone through all the questions.
+    if(this.currentIndex === this.questions.length) return null;
+
+    // Filling out the quiz card.
+    this.$.find('#question').text(this.current.question);
+    this.$.find('[data-opt="0"]').text(this.current.options[0]);
+    this.$.find('[data-opt="1"]').text(this.current.options[1]);
+    this.$.find('[data-opt="2"]').text(this.current.options[2]);
+    this.$.find('[data-opt="3"]').text(this.current.options[3]);
+  }
+
+  async show() {
+    await fadeIn(this.$);
     return this;
   }
 
-  reset() {
-    this.#timer.onEnd(() => {
-      this.flipOut()
-        .then(this.flipIn(this.#endHigh));
-    })
-    for(let question of this.questions)
-      question.answer = null;
+  async hide() {
+    await fadeOut(this.$);
+    return this;
   }
 }
 
-class JonDom {
-  constructor(selector) {
-    this.$(selector);
+class EndCard {
+  constructor() {
+    this.$ = $('#end')
   }
 
-  $(selector) {
-    this._el = document.querySelectorAll(selector);
+  async show() {
+    await fadeIn(this.$);
+    return this;
   }
 
-  forEach(callback) {
-    for(let i = 0; i < this._el.length; i++) {
-      let node = this._el[i];
-      callback(node, i, this._el);
-    }
-  }
-
-  addClass(...cls) {
-    this.forEach(node => node.classList.add(...cls));
-  }
-
-  removeClass(...cls) {
-    this.forEach(node => node.classList.remove(...cls));
-  }
-
-  hasClass(cls, callback) {
-    let hasClass = false;
-    this.forEach(node => {
-      if(node.classList.contains(cls)) {
-        callback(node);
-        hasClass = true;
-      }
-    })
-    return hasClass;
-  }
-
-  show() {
-    this.forEach(node => node.style.display = '');
-  }
-
-  hide() {
-    this.forEach(node => node.style.display = 'none');
-  }
-
-  on(eventName, eventListener) {
-    this.forEach(node => node.addEventListener(eventName, eventListener));
-  }
-
-  once(eventName, eventListener) {
-    function runMeOneTime(event) {
-      event.currentTarget.removeEventListener(eventName, runMeOneTime);
-      eventListener(event);
-    }
-    this.forEach(node => node.addEventListener(eventName, runMeOneTime));
-  }
-
-  onClick(eventListener) {
-    this.on('click', eventListener);
-  }
-
-  onClickOnce(eventListener) {
-    this.once('click', eventListener);
-  }
-
-  children(selector) {
-    let newJD = new JonDom('body');
-    newJD._el = this._el[0].querySelectorAll(selector);
-    return newJD;
-  }
-
-  text(text) {
-    this.forEach(node => node.innerText = text);
-  }
-
-  static _$(selector) {
-    return new JonDom(selector);
-  }
-
-  static DOMReady(callback) {
-    let stateReadyCheck = setInterval(() => {
-      if(document.readyState === 'complete') {
-        clearInterval(stateReadyCheck);
-        callback(this._$);
-      }
-    }, 10);
+  async hide() {
+    await fadeOut(this.$);
+    return this;
   }
 }
 
-class TimeController {
-  #runOnEnd;
-  constructor(_$) {
-    this._$ = _$;
-    this.current = _timer.start;
-    this.#runOnEnd = [];
-  }
+let score = 0;
+let timer = new TimeManagement();
+let quiz = new QuizCard(_questions);
+let endCard = new EndCard();
 
-  start() {
-    this.current = _timer.start;
-    this.interval = setInterval(this.tick.bind(this), _timer.interval);
-  }
-
-  tick() {
-    this.subtract(1);
-    this.write();
-    if(this.current === 0) this.stop();
-  }
-
-  onEnd(eventListener) {
-    this.#runOnEnd.push(eventListener);
-  }
-
-  stop() {
-    clearInterval(this.interval);
-    for(let eventListener of this.#runOnEnd) {
-      eventListener();
-    }
-  }
-
-  write() {
-    this._$.text(this.current);
-  }
-
-  subtract(amount) {
-    let newAmount = (this.current - amount);
-    this.current = newAmount <= 0 ? 0 : newAmount;
-  }
-}
-
-JonDom.DOMReady(_$ => {
-  const start = _$('#start');
-  const quiz = _$('#quiz');
-  const timer = new TimeController(_$('#timer'));
-  const endHigh = _$('#end-high');
-  const controller = new QuizManager(quiz, timer, endHigh);
-
-  controller.addQuestion(q1)
-    .addQuestion(q2)
-    .addQuestion(q3)
-    .addQuestion(q4)
-    .addQuestion(q5)
-    .updateScore(_$('#score'));
-  debugger;
-
-  start.children('.btn')
-    .onClickOnce(event => {
-      controller.start(start);
-      event.stopImmediatePropagation();
-    })
-
-  quiz.children('.btn')
-    .onClick(event => {
-      let el = event.currentTarget;
-      if(el.classList.contains('btn')) {
-        controller.answer = el.dataset.option;
-        event.stopImmediatePropagation();
-      }
-    })
+timer.onStart(el => {
+  el.parentElement.classList.add('bg-success');
+  el.parentElement.classList.remove('bg-danger');
 })
+
+timer.onEnd(el => {
+  el.parentElement.classList.add('bg-danger');
+  el.parentElement.classList.remove('bg-success');
+  quiz.hide()
+    .then(() => endCard.show());
+})
+
+function startQuiz() {
+  let start = $('#start');
+  timer.start();
+  quiz.display();
+  score = 0;
+  fadeOut(start)
+    .then(() => quiz.show());
+}
+
+function answerQuestion() {
+  // Converting {this} to jQuery.
+  const self = $(this);
+
+  // Getting the answer provided.
+  const answerGiven = self.attr('data-opt');
+
+  // Finding out if it is the correct answer.
+  provideMark(quiz.answerQuestion(answerGiven));
+
+  // Fading in and out.
+  quiz.hide()
+    .then(() => {
+
+      if(quiz.display() === null) {
+        // The onEnd for timer we are showing endCard.
+        return timer.stopTimer();
+      }
+      return quiz.show();
+    })
+}
+
+function provideMark(isCorrectAnswer) {
+  if(isCorrectAnswer) {
+    toast();
+  } else {
+    toast(false);
+    timer.subtractTime(10);
+  }
+}
+
+jQuery(function($) {
+  // Setting event for starting the quiz.
+  $('#start .btn').click(startQuiz)
+
+  // Setting event for answering a question.
+  $('#quiz .btn').click(answerQuestion)
+})
+
+/**
+ * This takes a jQuery element and fades it out using Bootstrap.  Bootstrap's fade is 150ms, a promise is returned.
+ * @param {jQuery} jQueryElement
+ * @returns {Promise<jQuery>}
+ */
+function fadeOut(jQueryElement) {
+  return new Promise(res => {
+    jQueryElement
+      .removeClass('show')
+      .addClass('fade')
+    setTimeout(() => {
+      jQueryElement.addClass('d-none');
+      res(jQueryElement);
+    }, 150)
+  })
+}
+
+/**
+ * Fades a jQuery element in using Bootstrap.  Bootstrap's fade is 150ms, a promise is returned.
+ * @param {jQuery} jQueryElement
+ * @returns {Promise<jQuery>}
+ */
+function fadeIn(jQueryElement) {
+  return new Promise(res => {
+    jQueryElement
+      .removeClass('d-none')
+      .addClass('show', 'fade')
+    setTimeout(() => res(jQueryElement), 150);
+  })
+}
+
+function toast(correct = true) {
+  const toast = jQuery(`<output></output>`);
+  toast
+    .addClass('toast text-white')
+    .addClass(!correct ? 'bg-danger' : 'bg-success')
+    .text(!correct ? 'Incorrect' : 'Correct')
+    .appendTo('#toast');
+  setTimeout(() => toast.remove(), 3300);
+}
